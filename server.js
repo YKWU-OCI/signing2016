88508cb39944b29d7298bf9ab272192f773656ee
Ying Ki Wu <wuyingki@hk.ibm.com>
2016-11-08 19:58:36 -0600

    add console log to server.js

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ var serveStatic = require('serve-static');
 var port = (process.env.VCAP_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 connect().use(serveStatic(__dirname)).listen(port,host);
+console.log("started at:" + host);
